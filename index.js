a939caea552d7fdc7d9ca24a441acaa745a49019
Junior R. Ribeiro <j5r@outlook.com>
2025-01-21 11:36:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function cabecalho(){
     /* li nome */
     var _a_nome = document.createElement("a")
     _a_nome.setAttribute("id","nome")
-    _a_nome.setAttribute("href","https://www.icmc.usp.br/pessoas?id=19450383")
+    _a_nome.setAttribute("href","https://graduacao.ufms.br/cursos/2202/professores")
     _a_nome.innerText = "Junior Rodrigues Ribeiro"
 
     var _li_nome = document.createElement("li")
